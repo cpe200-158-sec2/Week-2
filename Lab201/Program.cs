db708cff1c960514d5f075c7aef0648e78cd07b4
Watchakon <WatchakonP@users.noreply.github.com>
2015-08-31 23:39:01 +0700

    add Student Class
    (Student class is in the same file as mainClass)

--- Lab201/Program.cs
+++ Lab201/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Lab2
+namespace Student
 {
 	class MainClass
 	{
@@ -21,12 +21,69 @@ namespace Lab2
 				Console.WriteLine (s);
 
 			Console.WriteLine ("\n--- Playing with s4, s5, s6 objects ---");
-			stds [4].Name = "Mario";
-			stds [5].YearOfBirth = 1900;
-			stds [6].isActive = false;
+			stds [4].setName("Mario");
+			stds [5].setYob(1900);
+			stds [6].setActive(false);
 			for (int i = 4; i < stds.Length; i++)
 				Console.WriteLine (stds [i]);
-
+			Console.ReadLine();
+		}
+	}
+	
+	class Student{
+		private string name = "John Lennon";
+		private string stid = "Unknown";
+		private int yob = 1995;
+		private bool isActive = true;
+		//function
+		public Student(){
+			yob = 1995;
+			isActive = false;
+		}
+		public Student(string name, string stid){
+			this.name = name;
+			this.stid = stid;
+		}
+		public Student(string name, string stid, int yob){
+			this.name = name;
+			this.stid = stid;
+			this.yob = yob;
+		}
+		public Student(string name, string stid, int yob, bool action){
+			this.name = name;
+			this.stid = stid;
+			this.yob = yob;
+			this.isActive = action;
+		}
+		
+		public void setName(string name){
+			this.name = name;
+		}
+		public void setStid(string stid){
+			this.stid = stid;
+		}
+		public void setYob(int yob){
+			if (yob < 1950 || yob > 2000){
+				Console.WriteLine(stid+": Invalid year-of-birth value! (Only 1950-2000)");
+			}
+			else{ 
+				this.yob = yob; 
+			}
+		}
+		public void setActive(bool action){
+			isActive = action;
+		}
+		public int getAge(){
+			return 2015-this.yob;
+		}
+		
+		public override string ToString(){
+			if (isActive == true){
+				return "Student :"+name+"("+stid+")"+"age="+getAge() +" is active student";
+			}
+			else{
+				return "Student :"+name+"("+stid+")"+"age="+getAge() +" is not active student";
+			}
 		}
 	}
 }
